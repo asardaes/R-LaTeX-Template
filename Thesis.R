b7c0376e2469334d00be8b2679e607cff80676b3
asardaes <alexis.sarda@gmail.com>
2016-12-04 13:06:48 +0100

    Added default reproducibility functionality
    Conflicts:
    	Thesis.pdf
    	Thesis.tex

--- Thesis.R
+++ Thesis.R
@@ -1,29 +1,47 @@
 library(knitr)
+library(foreach)
 
+# Read arguments
 cmd_args <- commandArgs(TRUE)
-
 file_name <- cmd_args[1L]
-do_par <- as.logical(cmd_args[2L])
+do_par <- isTRUE(as.logical(cmd_args[2L]))
 
-if (isTRUE(do_par)) {
-     library(doParallel)
-
-     if (.Platform$OS.type == "windows") {
-          cl <- makeCluster(detectCores())
-     } else {
-          cl <- makeCluster(detectCores(), "FORK")
-     }
-
-     invisible(clusterEvalQ(cl, source("Utils/utils-nocache.R")))
-     invisible(clusterEvalQ(cl, source("Utils/utils.R")))
-
-     registerDoParallel(cl)
+# Load random seeds
+if (file.exists("Data/seeds.RData")) {
+    message("Loading existing seeds...")
+    load("Data/seeds.RData")
+} else {
+    message("Creating new seeds...")
+    seeds <- list()
 }
 
+# Helper functions that should always be loaded
+source("Utils/utils-nocache.R")
+
+# Create parallel workers
+if (do_par) {
+    library(doParallel)
+    
+    workers <- makeCluster(detectCores())
+    
+    invisible(clusterEvalQ(workers, source("Utils/utils-nocache.R")))
+    invisible(clusterEvalQ(workers, source("Utils/utils.R")))
+    
+    registerDoParallel(workers)
+    
+} else {
+    registerDoSEQ()
+}
+
+# Knit
 out <- try(knit(paste0(file_name, ".Rnw"), envir = new.env()))
 
-if ( inherits(out, "try-error") && file.exists(paste0(file_name, ".tex")) )
-     file.remove(paste0(file_name, ".tex"))
+# If there was an error, delete temporary .tex file
+if (inherits(out, "try-error") && file.exists(paste0(file_name, ".tex")))
+    file.remove(paste0(file_name, ".tex"))
 
-if (isTRUE(do_par))
-     stopCluster(cl)
+# Stop workers
+if (do_par) stopCluster(workers)
+
+# Save random seeds
+save(seeds, file = "Data/seeds.RData")
--- Utils/utils-nocache.R
+++ Utils/utils-nocache.R
@@ -3,56 +3,66 @@
 # ===========================================================================================================
 
 library(tools) # for md5sum()
-library(foreach) # for registerDoSEQ()
 
 # ===========================================================================================================
 # knitr hook to load utils if needed
 # ===========================================================================================================
 
 utils_hook <- function(before, options, envir) {
-     # Notice that path is relative to root file Thesis.Rnw
-     if (before) source("Utils/utils.R")
+    # Notice that path is relative to root file Thesis.Rnw
+    if (before) source("Utils/utils.R")
+    invisible(NULL)
+}
 
-     NULL
+# ===========================================================================================================
+# seeds hook to load and restore random seeds
+# ===========================================================================================================
+
+seeds_hook <- function(before, options, envir) {
+    # Notice that path is relative to root file Thesis.Rnw
+    if (before) {
+        if (is.null(seeds[[options$label]])) {
+            seeds[[options$label]] <<- .GlobalEnv$.Random.seed
+        } else {
+            assign(".Random.seed", seeds[[options$label]], globalenv())
+        }
+    }
+    
+    invisible(NULL)
 }
 
 # ===========================================================================================================
 # Custom MD5 function to make sure that the file exists
 # ===========================================================================================================
 
-my_md5sum <- function(path) {
-     hash <- tools::md5sum(path)
-
-     if (is.na(hash)) stop(paste("Couldn not find file:", as.character(path)))
-
-     hash
+md5 <- function(path) {
+    hash <- tools::md5sum(path)
+    if (is.na(hash)) stop(paste("Couldn not find file:", as.character(path)))
+    hash
 }
 
 # ===========================================================================================================
 # Custom cleaning function
 # ===========================================================================================================
 
-clean_workspace <- function(env = parent.frame(), exclude = character(0), load.utils = FALSE) {
-     vars <- ls(env)
-
-     exclude <- c("my_md5sum", "clean_workspace", exclude)
-     vars <- setdiff(vars, exclude)
-
-     rm(list = vars, pos = env)
-
-     # I only have two levels of folders, and this could be called from either
-     if (load.utils) {
-          if (file.exists("Utils/utils.R"))
-               source("Utils/utils.R", local = env)
-          else if (file.exists("../Utils/utils.R"))
-               source("../Utils/utils.R", local = env)
-          else
-               stop("Could not find utils.R")
-     }
-
-     gc()
-
-     invisible(TRUE)
+clean_workspace <- function(env = parent.frame(), exclude = character(0L), load.utils = FALSE) {
+    vars <- ls(env)
+    exclude <- c("md5", "clean_workspace", "seeds", exclude) # for parallel workers
+    vars <- setdiff(vars, exclude)
+    rm(list = vars, pos = env)
+    
+    # I only have two levels of folders, and this could be called from either
+    if (load.utils) {
+        if (file.exists("Utils/utils.R"))
+            source("Utils/utils.R", local = env)
+        else if (file.exists("../Utils/utils.R"))
+            source("../Utils/utils.R", local = env)
+        else
+            stop("Could not find utils.R")
+    }
+    
+    gc()
+    invisible(TRUE)
 }
 
 NULL
