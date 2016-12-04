35d83b35a9c2388036997e5d102f0571ba82f308
asardaes <alexis.sarda@gmail.com>
2016-12-04 13:06:48 +0100

    Added default reproducibility functionality

--- Utils/utils.R
+++ Utils/utils.R
@@ -13,35 +13,31 @@ library(randomForest) # for rf
 # ===========================================================================================================
 
 make_table <- function(df, ...) {
-     require(xtable)
-
-     dots <- list(...)
-
-     if (!is.null(dots$caption) && length(dots$caption) == 1L)
-          dots$caption <- c(dots$caption,
-                            strsplit(dots$caption, "[[:punct:]]")[[1]][1])
-
-     defaults <- list(
-          align = c("l", rep("c", ncol(df))),
-          label = paste0("tab:", as.character(substitute(df)), collapse = ""),
-          table.placement = "htbp",
-          caption.placement = "top",
-          caption.width = NULL,
-          booktabs = TRUE
-     )
-
-     if (!is.null(dots$include.rownames) && !dots$include.rownames)
-          defaults$align[2] <- "l"
-
-     defaults <- defaults[setdiff(names(defaults), names(dots))]
-
-     my_args <- c(list(x = df), defaults, dots)
-
-     table <- do.call("xtable", my_args)
-
-     my_args$x <- table
-
-     do.call("print.xtable", my_args)
+    require(xtable)
+    
+    dots <- list(...)
+    
+    if (!is.null(dots$caption) && length(dots$caption) == 1L)
+        dots$caption <- c(dots$caption, strsplit(dots$caption, "[[:punct:]]")[[1L]][1L])
+    
+    defaults <- list(
+        align = c("l", rep("c", ncol(df))),
+        label = paste0("tab:", as.character(substitute(df)), collapse = ""),
+        table.placement = "htbp",
+        caption.placement = "top",
+        caption.width = NULL,
+        booktabs = TRUE
+    )
+    
+    if (!is.null(dots$include.rownames) && !dots$include.rownames)
+        defaults$align[2L] <- "l"
+    
+    defaults <- defaults[setdiff(names(defaults), names(dots))]
+    my_args <- c(list(x = df), defaults, dots)
+    table <- do.call("xtable", my_args)
+    
+    my_args$x <- table
+    do.call("print.xtable", my_args)
 }
 
 NULL
